0bc499a6ed3c4f038c00f680224356230ee5e7f0
dychotomy <dychotomy2000@yahoo.com>
2020-07-16 12:10:05 -0400

    Added, Connection, Seed and song files

---
+++ models/song.js
@@ -0,0 +1,14 @@
+const mongoose = require('../db/connection');
+
+const songSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        artist: { type: String, required: true },
+        time: { type: Number, required: true },
+        isFavorite: { type: Boolean, default: false },
+    }
+)
+
+const Item = mongoose.model('Song', songSchema)
+
+module.exports = Song
